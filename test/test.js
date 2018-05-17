b459be7f05189ecd08ab23a6628b0dfc07d3f27b
Alexander Milevski <info@w8r.name>
2018-05-17 18:38:36 +0200

    added test, added git repo

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const {
   cubicBezierLine,
-  quadBezierAABB
+  quadBezierAABB,
+  cubicBezierAABB
 } = require('../dist/bezier-intersect');
 
 const { assert } = require('chai');
@@ -17,6 +18,28 @@ describe('intersectCubicBezierLine', () => {
     assert.isTrue(!!cubicBezierLine(0,0, 100, 100, 200, 100, 300, 0, 100, 0, 200, 100, result))
     assert.deepEqual(result, [173.20508075688772, 73.20508075688774]);
   });
+});
+
+describe('intersectCubicBezierAABB', () => {
+
+  it ('intersects', () => {
+    const x = 981.7516776530498, y = 1202.2380271093887;
+    const dx = 860.2542723247064, dy = 1323.735432437732;
+    const res = [];
+    const q = [843, 1228, 943, 1328];
+    assert.equal(cubicBezierAABB(x, y, dx, y, x, dy, x, y, q[0], q[1], q[2], q[3], res), 2);
+    assert.deepEqual(res, [ 927.7910501404327, 1228, 942.9999999999999, 1252.974906993395 ]);
+  });
+
+  it ('does not intersect', () => {
+    const x = 981.7516776530498, y = 1202.2380271093887;
+    const dx = 860.2542723247064, dy = 1323.735432437732;
+    const res = [];
+    const q = [843 - 100, 1228 - 100, 943 -100, 1328 - 100]
+    assert.equal(cubicBezierAABB(x, y, dx, y, x, dy, x, y, q[0], q[1], q[2], q[3], res), 0);
+    assert.deepEqual(res, []);
+  });
+
 });
 
 describe('quad bezier rectangle', () => {
